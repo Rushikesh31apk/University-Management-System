61910c1467b72f60482de478afcb46833c15fc04
Rushikesh31apk <narawaderushikesh@gmail.com>
2024-04-08 22:19:24 +0530

    first commit

---
+++ src/java_university/Conn.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_university;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "@Rushi31");
+            s = c.createStatement();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
